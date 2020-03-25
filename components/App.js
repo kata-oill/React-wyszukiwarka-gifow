706f4ed38e894972c89e252cc2b426a304469d31
Katarzyna Grabowska <kati.grab@gmail.com>
2020-03-25 20:57:19 +0100

    Modify function

--- components/App.js
+++ components/App.js
@@ -21,6 +21,8 @@ App = React.createClass({
     },
 
     getGif: function(searchingText, callback) { 
+        var GIPHY_API_URL = 'https://api.giphy.com';
+        var GIPHY_PUB_KEY = 'nRwKqqPVy1FiB6f1wnCaDUdYfkmAlnta';
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
         var xhr = new XMLHttpRequest();  
         xhr.open('GET', url);
